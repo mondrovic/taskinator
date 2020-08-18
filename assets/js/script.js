b50b67f3a24d11409a87b16699560efc403b49b0
Mondrovic <mondrovic1@gmail.com>
2020-08-17 19:59:12 -0700

    refactored

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(){
+var taskFormHandler = function(){
 
     // overides browser default behavior
     event.preventDefault();
@@ -10,21 +10,32 @@ var createTaskHandler = function(){
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     // selects select type with same name task-type
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // package up data as an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    createTaskEl(taskDataObj);
+};
+
+var createTaskEl = function(taskDataObj){
+
+        // create list item
+        var listItemEl = document.createElement("li");
+        listItemEl.className = "task-item";
     
-    // create list item
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-
-    // create div to hold task info and add  to list item
-    var taskInfoEl = document.createElement("div");
-    taskInfoEl.className = "task-info";
-    // add html to div
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
-
-    listItemEl.appendChild(taskInfoEl);
-
-    tasksToDoEl.appendChild(listItemEl);
-}
+        // create div to hold task info and add  to list item
+        var taskInfoEl = document.createElement("div");
+        taskInfoEl.className = "task-info";
+        // add html to div
+        taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+    
+        listItemEl.appendChild(taskInfoEl);
+    
+        tasksToDoEl.appendChild(listItemEl);
+};
 
 // adds event listener to button and appends list items
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
